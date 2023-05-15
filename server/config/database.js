2f8447772d2b5cbaffbb6604b58281cb3dea9ec9
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2023-05-15 20:28:12 +0530

    set

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connect = () =>{
    mongoose.set('strictQuery', false);
-   console.log(process.env.MONGO_URL)
+
    mongoose.connect(`${process.env.MONGO_URL}`,  {
       useNewUrlParser: true,
       useUnifiedTopology: true
