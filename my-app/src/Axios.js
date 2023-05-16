46e62db773f68bff253a251dd5b09405ec2a4700
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2023-05-16 09:43:02 +0530

    set

--- my-app/src/Axios.js
+++ my-app/src/Axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const Axios = axios.create({
-    baseURL : "http://localhost:5000/",
+    // baseURL : "http://localhost:5000/",
+    baseURL : 'https://flipkart-clone-bay.vercel.app/',
     withCredentials:true
 })
