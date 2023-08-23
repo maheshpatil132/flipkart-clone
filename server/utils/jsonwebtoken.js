4d33509c0dbacaba15501dddd1e1c159a289e5ff
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2023-08-23 15:11:55 +0530

    fix bugs in cookie

--- server/utils/jsonwebtoken.js
+++ server/utils/jsonwebtoken.js
@@ -9,7 +9,7 @@ exports.sendtoken = (user, status , res)=>{
         expires: new Date( Date.now() + process.env.Cookie_Expire * 60 * 60 * 24 * 1000),
         secure: true,
         httpOnly: true,
-        sameSite: 'lax'
+        sameSite: 'none'
     }
 
     res.status(status).cookie('token', token , options).json({
