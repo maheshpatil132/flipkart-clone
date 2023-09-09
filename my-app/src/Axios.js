7586d4e26d6beaf0536c8b1973fc855fbe21435f
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2023-09-09 12:15:15 +0530

    test

--- my-app/src/Axios.js
+++ my-app/src/Axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
+// For Production, Create .env file and put your backend api URL in that, OR use localhost url
 export const Axios = axios.create({
     baseURL : process.env.REACT_APP_BACKEND_URL,
-    // baseURL : 'https://flipkart-api.vercel.app/',
+    // baseURL : 'http://localhost:5000/',
     withCredentials:true
 })
