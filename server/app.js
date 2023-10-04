fe5cb8cab3af59c27dc3828124b034e6955c61a3
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2023-10-04 11:27:31 +0530

    small change

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const corsOptions ={
-    origin:'https://flipkart-clone-ui.vercel.app', 
+    origin:'*', 
     // origin:'http://localhost:3000', 
     credentials:true,            
     optionsSuccessStatus: 200,
